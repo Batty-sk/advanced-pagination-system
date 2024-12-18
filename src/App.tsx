3f5c35c3e97505e0af61dcc03d4efc491676ecb3
Batty-sk <saorav.skumar@gmail.com>
2024-12-18 19:10:39 +0530

    added: api fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { DataTable } from "primereact/datatable";
+import { Paginator, PaginatorPageChangeEvent } from 'primereact/paginator';
 import { Column } from "primereact/column";
+import { Checkbox } from 'primereact/checkbox';
 
-const App = () => {
+
+interface apiDataInterface {
+  title:string,
+  place_of_origin:string, 
+  artist_display: string,
+  inscriptions:string | null,
+  date_start:number,
+  date_end:number
+}
+
+const App = () => { 
+  const [loading,updateLoading] = useState<boolean>(false)
+  const [checkedData,updateCheckedData] = useState<Map<number,number>>(new Map())
+  const [apiData,updateApiData] = useState<apiDataInterface[]>();
+  const [pgNo,updatepgNo]  = useState<number>(0)
+  const [rows, setRows] = useState<number>(10);
+
+
+  useEffect(()=>{
+   
+   updateLoading(true)
+   fetch(`https://api.artic.edu/api/v1/artworks?page=${pgNo+1}`).then(res=>{
+    if(res.ok){
+      return res.json()
+    }
+   })
+   .then(data => {console.log('getting data',data);updateLoading(false);updateApiData(data.data)})
+   .catch(error=>{console.log("something went wrong.",error); updateLoading(false)})
+    
+
+  },[pgNo])
+
+
+  const onPageChange = (event:PaginatorPageChangeEvent)=>{
+    console.log('event page ',event.first)
+    setRows(event.rows);
+    updatepgNo(event.first)
+  }
   return (
-    <main className="text-3xl font-bold min-h-svh bg-[#f5ebe0]  text-black flex flex-col justify-center items-center font-mono ">
-      <h1>Advanced Pagination Example </h1>
-      <div className=" mt-10 text-2xl shadow-md shadow-black">
+    <main className="text-3xl font-bold w-full min-h-svh bg-[#f5ebe0]  text-black flex flex-col justify-center items-center font-mono ">
+      <h1>Advanced Pagination </h1>
+      <div className=" mt-10 text-2xl min-w-96 w-11/12 shadow-md shadow-black">
       <DataTable
-        value={[
-          { name: "batman", code: "1001", category: "Indian" },
-          { name: "batman", code: "1001", category: "Indian2" },
-          { name: "batman", code: "1001", category: "Indian3" },
-        ]}
-         className="border min-w-96 min-h-40 p-4 bg-[#e3d5ca] "
-         columnResizeMode={'expand'}
-         loading={true}
+        value={apiData}
+         className="border min-w-96 w-full min-h-40 p-4 bg-[#e3d5ca]"
+         loading={false}
+         rowHover={true}
          
       > 
-    <Column field="code" header="Code" className="text-1xl font-normal"></Column>
-    <Column field="name" header="Name" className="text-1xl font-normal"></Column>
-    <Column field="category" header="Category" className="text-1xl font-normal"></Column>
+ //title, place_of_origin, artist_display, inscriptions, date_start, date_end
+
+
+    <Column field="title" header="Title" className="text-[0.9rem] font-normal hover:bg-[#d5bdaf] cursor-pointer w-80 overflow-clip "></Column>
+    <Column field="place_of_origin" header="Place" className="text-[0.9rem] font-normal hover:bg-[#d5bdaf] cursor-pointer "></Column>
+    <Column field="artist_display" header="Artist" className="text-[0.9rem] font-normal hover:bg-[#d5bdaf] cursor-pointer overflow-clip "></Column>
 
     </DataTable>
+    <Paginator first={pgNo} rows={rows} totalRecords={100} onPageChange={onPageChange} />
+
     </div>
     </main>
   );
